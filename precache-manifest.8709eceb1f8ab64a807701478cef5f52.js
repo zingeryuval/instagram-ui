b5b89a47b8f1cc4c41d3658ccc430d0a2ba854ad
Yuval Zinger <zingeryuval@gmail.com>
2019-11-24 15:43:19 +0200

    Updates

---
+++ precache-manifest.8709eceb1f8ab64a807701478cef5f52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3847dc7b093893e5926f5ac969bd5076",
+    "url": "/instagram-ui/index.html"
+  },
+  {
+    "revision": "013e9a3047178c16c34e",
+    "url": "/instagram-ui/static/css/main.ac4a2f40.chunk.css"
+  },
+  {
+    "revision": "374a0ed557cf36ce9911",
+    "url": "/instagram-ui/static/js/2.f28a7e40.chunk.js"
+  },
+  {
+    "revision": "013e9a3047178c16c34e",
+    "url": "/instagram-ui/static/js/main.0d778b35.chunk.js"
+  },
+  {
+    "revision": "fd329e52972bd349eb48",
+    "url": "/instagram-ui/static/js/runtime-main.9b17314b.js"
+  }
+]);
